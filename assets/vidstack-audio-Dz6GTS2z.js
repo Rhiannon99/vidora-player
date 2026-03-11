23d2359b052e1eae832a0193241422b0946565a5
Rhiannon99 <rhiannon99@users.noreply.github.com>
2026-03-11 08:43:29 +0100

    v1.0.6: fix display:block with !important to beat Vidstack base CSS specificity

--- assets/vidstack-audio-Dz6GTS2z.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./vidstack-html-BY20rY41.js";import"./index-qoUvPcZI.js";import"./vidstack-BhYx9Fjk-D1wPm1Mg.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(){super.setup(),this.type==="audio"&&this.b.delegate.c("provider-setup",this)}get audio(){return this.a}}export{o as AudioProvider};
