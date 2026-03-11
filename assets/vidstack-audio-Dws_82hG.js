4ad4a0a7bc00ba12ff8b26d2eaf38552de96367f
Rhiannon99 <rhiannon99@users.noreply.github.com>
2026-03-11 09:02:29 +0100

    v1.0.7: fix chunk 404s by using base:./ so CDN chunks resolve correctly

--- assets/vidstack-audio-Dws_82hG.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./vidstack-html-DSSSiT20.js";import"./index-k4jfbbot.js";import"./vidstack-BhYx9Fjk-ZeXDGKss.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(){super.setup(),this.type==="audio"&&this.b.delegate.c("provider-setup",this)}get audio(){return this.a}}export{o as AudioProvider};
